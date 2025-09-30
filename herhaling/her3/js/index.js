ab08223e6ed889bbc1b45fe574ae092ea9cce626
Magaly Bossuyt <bossuytmagaly2@gmail.com>
2025-09-30 08:54:36 +0200

    commit

---
+++ herhaling/her3/js/index.js
@@ -0,0 +1,7 @@
+/*gebruik een for-loop om de even getallen van 0 tot 20 te printen
+ */
+for (let i=1;i<=20;i++){
+    if (i%2 ===0){
+        console.log(i);
+    }
+}
--- oefenbundel2/oef4 for-loop/js/index.js
+++ oefenbundel2/oef4 for-loop/js/index.js
@@ -1,17 +1,5 @@
-/*Schrijf een programma die de faculteit van een getal weergeeft.
-*Vraag dit getal aan de gebruiker
- */
-let getal=parseInt(window.prompt("Geef een getal in"));
+/*Schrijf een programma die je alle poteniële mogelijke geeft in 2-decimale vorm uitprint met een komma er tussen tot en met 99 */
 
-function berekenFaculteit(getal){
-    if (getal<0){
-        return "faculteit word niet gedefinieerd"
-    }
-    let faculteit=1;
-     for (let i=1;i<=getal;i++){
-         faculteit *= i;
-    }
-     return faculteit
+for (let i=1;i<=100;i++){
+
 }
-let resultaat=berekenFaculteit(getal);
-console.log(`De facultiet van ${getal} is ${resultaat}`)
